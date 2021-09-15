34c58096185bdab815e7b6edaf66c18e3e339e6a
Glitch (buttercup-educated-dracopelta) <none>
2021-09-15 16:06:18 +0000

    🏃🐬 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -3,10 +3,26 @@ var app = express();
 
 console.log("Hello World");
 
-app.use("/public", express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use(function (req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip)
+  next()
+})
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html")
 })
 
- module.exports = app;
+app.get("/json", function(req, res) {
+  var text = "Hello json";
+  var response = "";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = text.toUpperCase();
+  } else {
+    response = text;
+  }
+  res.json({"message": response});
+})
+
+module.exports = app;
