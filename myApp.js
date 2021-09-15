8b68b18fdbfa27caada7b7db434cef1cc519415c
Glitch (buttercup-educated-dracopelta) <none>
2021-09-15 15:35:07 +0000

    🐆💨 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -1,39 +1,12 @@
 var express = require('express');
 var app = express();
 
+console.log("Hello World");
 
+app.use("/public", express.static(__dirname + "/public"))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html")
+})
 
  module.exports = app;
